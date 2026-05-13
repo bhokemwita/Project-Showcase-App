f5cfde39715cc653f9f6065c3d0fff43b22701dc
bhokenatasha810@gmail.com <bhokenatasha810@gmail.com>
2026-05-13 11:36:01 +0300

    feat: implement routing with react-router, add new pages and context provider, and remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home";
+import {About} from "./pages/About";
+import {Landing} from "./pages/Landing";
+import {ProductProvider} from "./context/ProductContext";
+import {AdminPortal} from "./pages/AdminPortal";
+import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <ProductProvider>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/admin" element={<AdminPortal />} />
+        </Routes>
+      </div>
+    </ProductProvider>
   );
 }
-
 export default App;
+
